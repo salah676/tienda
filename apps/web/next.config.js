07adbd3d0516959d2a0be7a9f8eb0f8109204f42
salah676 <msalah44626@gmail.com>
2026-05-18 07:56:41 +0100

    fix: Clean up configuration files

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@debt-manager/db', '@debt-manager/types', '@debt-manager/utils'],
 }
 
 module.exports = nextConfig
